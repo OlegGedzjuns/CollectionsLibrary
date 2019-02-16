0cd12407e6e4208574ce5430e58f914c5cac4c52
SecyrB <oleggedzjuns@gmail.com>
2019-02-16 22:21:13 +0200

    Many changes : Changed : LinkedList_class methodes and encapsulation PushBack methode Added : ListElement_class PushFront and PushAt methodes

--- Collections/Collections.h
+++ Collections/Collections.h
@@ -1,43 +1,106 @@
 #pragma once
 namespace LinkedList
 {
-	template<class T>
-	class LinkedList_class
+template <class T>
+	class ListElement_class
 	{
 	private:
 		T _data;
-		int _size = 0;
-		LinkedList_class* _next = nullptr;
-		LinkedList_class* _tail = this;
+		ListElement_class* _next = nullptr;
 	public:
 		T& Data() { return _data; }
-		LinkedList_class*& Next() { return _next; }
-		LinkedList_class*& Tail() { return _tail; }
+		ListElement_class*& Next() { return _next; }
 
-		LinkedList_class()
+		ListElement_class(T data)
 		{
-
+			Data() = data;
 		}
+		ListElement_class(T data, ListElement_class* next)
+		{
+			Data() = data;
+			Next() = next;
+		}
+	};
 
-		T& PushBack(T data)
+template<class T>
+	class LinkedList_class
+	{
+	private:
+
+		ListElement_class<T>* _lastPtr = nullptr;
+		int _lastPos = -1;
+		ListElement_class<T>* _tail = nullptr;
+		ListElement_class<T>* _head = nullptr;
+		int _size = 0;
+	public:
+		ListElement_class<T>*& Tail() { return _tail; }
+		ListElement_class<T>*& Head() { return _head; }
+
+		ListElement_class<T>* PushBack(T data)
 		{
 			if (_size == 0)
 			{
-				Data() = data;
+				ListElement_class<T>* item = new ListElement_class<T>(data);
 				_size++;
-				return Data();
+				Head() = item;
+				Tail() = item;
+				return item;
 			}
 			else
 			{
-				Tail()->Next() = new LinkedList_class<T>;
+				Tail()->Next() = new ListElement_class<T>(data);
 				Tail() = Tail()->Next();
-				Tail()->Data() = data;
 				_size++;
-				return Tail()->Data();
+				return Tail();
 			}
 			
 		}
-		//T& PushFront()
+		ListElement_class<T>* PushFront(T data)
+		{
+			if (_size == 0)
+			{
+				return PushBack(data);
+			}
+			else
+			{
+				ListElement_class<T>* item = new ListElement_class<T>(data);
+				item->Next() = Head();
+				Head() = item;
+				_size++;
+			}
+		}
+		ListElement_class<T>* PushAt(int pos, T data)
+		{
+			if (pos <= 0)
+			{
+				PushFront(data);
+			}
+			else if (pos >= _size)
+			{
+				PushBack(data);
+			}
+			else
+			{
+				if (_lastPos <= pos - 1 && _lastPos != -1)
+				{
+					while (_lastPos < pos - 1)
+					{
+						_lastPtr = _lastPtr->Next();
+						_lastPos++;
+					}
+					ListElement_class<T>* item = new ListElement_class<T>(data, _lastPtr->Next());
+					_lastPtr->Next() = item;
+					_size++;
+					return item;
+				}
+				else
+				{
+					_lastPtr = Head();
+					_lastPos = 0;
+					return PushAt(pos, data);
+				}
+			}
+		}
 		int GetSize()
 		{
 			return _size;
