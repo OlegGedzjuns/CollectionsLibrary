d25f6f809766fdc5dc65499b0ab53cfd73aeb6cd
SecyrB <oleggedzjuns@gmail.com>
2019-02-17 13:26:42 +0200

    Many changes : **Removed :** ListElement bug **Changed :** PushAt argument name **Added :** PopBack, PopFront and PopAt methodes

--- Collections/Main.cpp
+++ Collections/Main.cpp
@@ -7,6 +7,13 @@ using namespace std;
 int main()
 {
 	LinkedList_class<int>* myList = new LinkedList_class<int>;
+	for (int i = 0; i < 10; i++)
+	{
+		myList->PushBack(i);
+	}
+	myList->PopAt(0);
+	myList->PopAt(5);
+	myList->PopAt(10);
 	for (auto it = myList->Head(); it != nullptr; it = it->Next())
 	{
 		cout << it->Data() << " ";
